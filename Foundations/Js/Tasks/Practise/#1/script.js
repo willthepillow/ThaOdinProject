0468782e5b457f861b62dabcef0a8b259c495601
Will <68452517+willthepillow@users.noreply.github.com>
2022-02-22 14:02:21 +0000

    changed script.js to call the functions

--- Foundations/Js/Tasks/Practise/#1/script.js
+++ Foundations/Js/Tasks/Practise/#1/script.js
@@ -1,8 +1,12 @@
+// calling all of these functions - when the page is refreshed it runs all of the tasks required
+
 function add7() {
     const a = window.prompt("Number?(to add to 7) ");
     sum = +a + +7; //appending the variable / number with "+" makes it a number and not a string
     alert(sum);
 }
+add7()
+
 
 function multiply() {
     let num1 = window.prompt("What is the first number?");
@@ -10,20 +14,20 @@ function multiply() {
     sum2 = num1 * num2;
     alert(sum2);
 }
+multiply()
 
 function capitalise() {
     let stringUncapped = window.prompt("What is the word that you would like the first word capitalised?");
     const stringCapped = stringUncapped.charAt(0).toUpperCase();
     alert(stringCapped + (stringUncapped.slice(1)) );
-
 }
+capitalise()
 
 function lastLetter() {
     let stringWhole = window.prompt("What is the word you would like the last letter to be shown?");
     const stringLast = stringWhole.charAt(stringWhole.length-1);
     alert(stringLast)
 }
-
-// to run these functions, jut call them - e.g. to run the capitalise function, write this "capitalise()"
+lastLetter()
 
 /* lesson has been completed */
