492d021b0a054a29c5435ea931843e4b76e3991f
Will <68452517+willthepillow@users.noreply.github.com>
2022-02-22 14:38:50 +0000

    added the first part for the fizzbuzz task - the solutions is on the same page as the task

--- Foundations/Js/Tasks/Fizzbuzz/fizzbuzz.js
+++ Foundations/Js/Tasks/Fizzbuzz/fizzbuzz.js
@@ -1,3 +1,14 @@
 function question () {
-const question1 = window.prompt("What is the first number")
+let answer = prompt("What is the first number");
 }
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    }else {
+            console.log(i);
+    }
+}
+
+// run through this later - The Odin Project runs through this anyways,
+// loop is pretty simple to understand - just need to learn the syntax for it
