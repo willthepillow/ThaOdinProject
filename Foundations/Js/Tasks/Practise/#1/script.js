eb029caf27fd24fe2fcaaa9c2a48d72167568dd3
Will <68452517+willthepillow@users.noreply.github.com>
2022-02-22 08:58:25 +0000

    Updated the first task to not call any functions

--- Foundations/Js/Tasks/Practise/#1/script.js
+++ Foundations/Js/Tasks/Practise/#1/script.js
@@ -3,7 +3,6 @@ function add7() {
     sum = +a + +7; //appending the variable / number with "+" makes it a number and not a string
     alert(sum);
 }
-add7()
 
 function multiply() {
     let num1 = window.prompt("What is the first number?");
@@ -11,7 +10,6 @@ function multiply() {
     sum2 = num1 * num2;
     alert(sum2);
 }
-multiply()
 
 function capitalise() {
     let stringUncapped = window.prompt("What is the word that you would like the first word capitalised?");
@@ -19,13 +17,13 @@ function capitalise() {
     alert(stringCapped + (stringUncapped.slice(1)) );
 
 }
-capitalise()
 
 function lastLetter() {
     let stringWhole = window.prompt("What is the word you would like the last letter to be shown?");
     const stringLast = stringWhole.charAt(stringWhole.length-1);
     alert(stringLast)
 }
-lastLetter()
 
-/* lesson has been completed  -hell yeah! */
+// to run these functions, jut call them - e.g. to run the capitalise function, write this "capitalise()"
+
+/* lesson has been completed */
