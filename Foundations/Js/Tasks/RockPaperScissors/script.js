2145785f5367305f46a51ad6311343cb87977e92
willthepillow <willthepillow8@gmail.com>
2022-02-23 22:29:31 +0000

    this is just a test

---
+++ Foundations/Js/Tasks/RockPaperScissors/script.js
@@ -0,0 +1,17 @@
+
+function computerPlay(randomRPS) {
+    console.log("bruh moment");
+    const rockPaperScissors = ["Rock", "Paper", "Scissors"]
+    let randomRPS = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)] // this line chooses between "rock", "paper" or "scissors" at random
+    console.log(randomRPS)
+}
+
+function userPlay() {
+    let userInput = window.prompt("Please input Either rock, paper or scissors.")
+    let userInputLowerCase = userInput.toLowerCase()
+    console.log(userInputLowerCase)
+}
+
+function game() {
+
+}
