3f317707b5e86121f751d20d32c3849ccc194375
willthepillow <willthepillow8@gmail.com>
2022-02-26 22:21:28 +0000

    Finally done the rock, paper, scissors task

--- Foundations/Js/Tasks/RockPaperScissors/script.js
+++ Foundations/Js/Tasks/RockPaperScissors/script.js
@@ -1,17 +1,51 @@
 
-function computerPlay(randomRPS) {
-    console.log("bruh moment");
-    const rockPaperScissors = ["Rock", "Paper", "Scissors"]
-    let randomRPS = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)] // this line chooses between "rock", "paper" or "scissors" at random
-    console.log(randomRPS)
+let computerPlay = () => {
+    let Selection = ["rock", "paper", "scissors"]
+    let computerInput = Selection[Math.floor(Math.random()*Selection.length)]
+    console.log(computerInput)
+    return computerInput;
 }
 
-function userPlay() {
-    let userInput = window.prompt("Please input Either rock, paper or scissors.")
-    let userInputLowerCase = userInput.toLowerCase()
-    console.log(userInputLowerCase)
+let userPlay = () => {
+    let userInput = window.prompt("rock, paper or scissors?");
+    userInput.toLowerCase()
+    console.log(userInput)
+    return userInput;
 }
 
-function game() {
+const userSelection = userPlay();
+const computerSelection = computerPlay();
 
+function playRound(userSelection, computerSelection) {
+    if(userSelection === computerSelection){
+        return "It's a draw";
+    }
+
+    else if(userSelection==="rock"){
+        if(computerSelection==="scissors"){
+            return "rock beats scissors, you win!";
+        }
+        else if(computerSelection==="paper"){
+            return "paper beats ... idk - you loose";
+        }
+    }
+
+    else if(userSelection==="paper"){
+        if(computerSelection==="scissors"){
+            return "You lost, scissors beats paper - unlucky!!";
+        }
+        else if(computerSelection==="rock"){
+            return "You lost, rock beats paper - HAHAHA!!!";
+        }
+    }
+
+    else if (userSelection==="scissors"){
+        if(computerSelection==="paper"){
+            return "you lucky ..., scissors beats paper";
+        }
+        else if (computerSelection==="rock"){
+            return "rock beats scissors";
+        }
+    }
 }
+console.log(playRound(userSelection, computerSelection));
