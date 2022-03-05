a7d4469380322c9da46c757f2bc5bfc580025c3d
willthepillow <willthepillow8@gmail.com>
2022-03-05 17:42:39 +0000

    Removed the comments for the script.js file as it improved readablility for me -- will add this back or re-do the comments at a later date

--- Foundations/Js/Tasks/RPS-ui/script.js
+++ Foundations/Js/Tasks/RPS-ui/script.js
@@ -4,18 +4,16 @@ let computerPlay = () => {
     let computerInput = Selection[Math.floor(Math.random()*Selection.length)]
     console.log(computerInput)
     return computerInput;
-}                           //The function above selects a random string from the array(rock, paper, scissors) and then returns it as 'computerInput'
+}                           
 
 let userPlay = () => {
-    let userInput = window.prompt("rock, paper or scissors?");
     userInput.toLowerCase()
     console.log(userInput)
     return userInput;
-}                         /* The funtion above asks the user to input what they want to type from rock, paper, scissors - if they capitalise anything or do not input the exact 
-string then the rest of the script will not work*/
+}                         
 
 const userSelection = userPlay();
-const computerSelection = computerPlay(); //This just transfers the variables used into different names so its easyer to type
+const computerSelection = computerPlay();
 
 function playRound(userSelection, computerSelection) {
     if(userSelection === computerSelection){
@@ -31,7 +29,7 @@ function playRound(userSelection, computerSelection) {
     }
     else if(userSelection==="paper"){
         if(computerSelection==="scissors"){
-            return "You lost, scissors beats paper - unlucky!!";          /*The function to the left just runs throught if / else if statements to play rock, paper, scissors */
+            return "You lost, scissors beats paper - unlucky!!";
         }
         else if(computerSelection==="rock"){
             return "You lost, rock beats paper - HAHAHA!!!";
