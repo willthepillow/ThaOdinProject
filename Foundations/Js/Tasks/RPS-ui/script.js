e5e83790c98fddffaeee5c671b24468c34e65f0c
Will <68452517+willthepillow@users.noreply.github.com>
2022-03-11 13:08:14 +0000

    Added DOM to RPS-ui else-if function, this is only for if rock wins at the minute as it may not work as intended - also added more html & css elements / styles for the UI element of this branch, things do need to look better, but the core functionality takes priority

--- Foundations/Js/Tasks/RPS-ui/script.js
+++ Foundations/Js/Tasks/RPS-ui/script.js
@@ -4,7 +4,7 @@ function userPlay () {
   btns.forEach(btns =>{
     btns.addEventListener("click", function(){
       const userInput = btns.value;
-      return userInput;;
+      return userInput;
     })
   })
 }
@@ -15,17 +15,28 @@ let computerPlay = () => {
   let computerInput = Selection[Math.floor(Math.random()*Selection.length)];
   console.log("computer input:  " + computerInput);
   return computerInput;
-}                                                   
+}                             
+
+
 const userSelection = userPlay();
 const computerSelection = computerPlay();
- 
+
+
+
+// Need to add DOM to this func so that it outputs in the UI rather than in the terminal - 
+//not sure how this is going to turn out - 
+//might add another function to incoroporate DOM with the if & else if statements below:
 function playRound(userSelection, computerSelection) {
   if(userSelection === computerSelection){
       return "It's a draw";
   }
   else if(userSelection==="rock"){
       if(computerSelection==="scissors"){
+          const output = output.getElemetById('game-out'){
+            output.innerHTML = "rock beats scissors, you win!";
+          } //this may not work as I can't test this in school - it looks like it should tho...
           return "rock beats scissors, you win!";
+
       }
       else if(computerSelection==="paper"){
           return "paper beats ... idk - you loose";
