f6981b94393a61aa20bf3a59b81175deac7198f6
willthepillow <willthepillow8@gmail.com>
2022-03-14 20:48:17 +0000

    useless, but this will be the last commit on this repo

--- Foundations/Js/Tasks/javascript-exercises/04_-notMyCode_removeFromArray/removeFromArray.js
+++
@@ -1,6 +0,0 @@
-const removeFromArray = function(value) {
-    return value.removeFromArray(value, 3)
-};
-
-// Do not edit below this line
-module.exports = removeFromArray();
--- Foundations/Js/Tasks/javascript-exercises/05_sumAll/sumAll.js
+++
@@ -1,8 +0,0 @@
-const sumAll = function(value1, value2) {
-    let totalSum = sumAll(value1, value2)
-    console.log(totalSum)
-    return(totalSum)
-};
-
-// Do not edit below this line
-module.exports = sumAll;
